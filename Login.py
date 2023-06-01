403305acfa14c4a2365bf334f21afef8148bd146
Anthony Alteirac <105443206+sfc-gh-aalteirac@users.noreply.github.com>
2023-06-01 11:15:25 +0200

    fix logout

--- Login.py
+++ Login.py
@@ -31,8 +31,9 @@ def getPage():
     st.session_state["username"]=username
 
     if authentication_status:
-        authenticator.logout('Logout', 'main')
-        st.write(f'Welcome *{name}*')
+        print('logout')
+        # authenticator.logout('Logout', 'main')
+        # st.write(f'Welcome *{name}*')
     elif authentication_status == False:
         st.error('Username/password is incorrect')
     elif authentication_status == None:
